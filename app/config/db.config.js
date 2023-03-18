b44e0b2cd0a2c0eafa7d00f0b0a22e95d5e80535
anshii03 <anshii03@github.com>
2023-03-18 14:44:00 +0530

    Update db.config.js

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    url: "mongodb+srv://anshikaagarwal92:Geeks123@cluster0.pdbmw9c.mongodb.net/?retryWrites=true&w=majority"
+    url: "mongodb+srv://newuser:qwerty123@cluster0.pdbmw9c.mongodb.net/?retryWrites=true&w=majority"
 }
